9204eb2a512c6024157f9ed45b08fa0db3c316df
griffithmatthew117 <griffithmatthew117@gmail.com>
2018-03-27 13:04:28 -0700

    please work

--- server/app.js
+++ server/app.js
@@ -3,16 +3,22 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-mongoose.connect('mongodb://Masttehen:MC1ef11!@ds125479.mlab.com:25479/blogapi');
+
+// let url = 'mongodb://Masttehen:MC1ef11!@ds125479.mlab.com:25479/blogapi'
+let url = 'mongodb://heroku_f9lh48hb:9jppsllql8buha75qbevt7tago@ds125489.mlab.com:25489/heroku_f9lh48hb';
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/my-blog");
 mongoose.Promise = Promise;
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB Connection Error"));
 
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 app.get('/', (req, res) => {
-    res.status(200).send();
-    res.send('I am the man')
+    res.status(200).send("go to /api/blogs or api/users");
+    // res.send('I am the man')
 })
 
 app.use('/api/users', require('./routes/users'));
